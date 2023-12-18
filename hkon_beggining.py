e64965f3176ea66c9f52ee7e5fff73b8ebe42456
Hkon Ellingsen <hakonellingsen2006@gmail.com>
2023-12-18 10:09:37 +0100

    added the file

---
+++ hkon_beggining.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+model = models.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(10, activation='softmax'),
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=1)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f'Test accuracy: {test_acc}')
